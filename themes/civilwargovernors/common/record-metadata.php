d9f2a4ed492957609ae92703a9692ba6b3288f01
Dazhi Jiao <djiao@jhu.edu>
2017-10-30 09:23:46 -0400

    Refactor code to filter and display metadata

--- themes/civilwargovernors/common/record-metadata.php
+++ themes/civilwargovernors/common/record-metadata.php
@@ -1,21 +1,24 @@
+<?php
+$blacklists = array();
+$blacklists['CWG Documents'] = array("Document Title", "Source Country", "Source State", "Source City", "ISO Date of Creation", "DocTracker Number", "Transcription");
+$blacklists['CWGK Person Item Type Metadata'] = array("Name", "Bibliography", "Biographical Text");
+?>
 <?php foreach ($elementsForDisplay as $setName => $setElements): ?>
-    <?php if ($setName == "CWG Documents"){ ?>
-        <div class="element-set">
-            <?php if ($showElementSetHeadings): ?>
-                <h2><?php echo html_escape(__($setName)); ?></h2>
-            <?php endif; ?>
-
-            <?php foreach ($setElements as $elementName => $elementInfo): ?>
-                <?php $blacklist = array("Document Title", "Source Country", "Source State", "Source City", "ISO Date of Creation", "DocTracker Number", "Transcription"); ?>
-                <?php if ( !in_array($elementName, $blacklist, true ) ) { ?>
-                    <div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
-                        <h3><?php echo html_escape(__($elementName)); ?></h3>
-                        <?php foreach ($elementInfo['texts'] as $text): ?>
-                            <div class="element-text"><?php echo $text; ?></div>
-                        <?php endforeach; ?>
-                    </div><!-- end element -->
-                <?php } ?>
-            <?php endforeach; ?>
-          </div><!-- end element-set -->
-      <?php } ?>
+  <div class="element-set">
+    <?php if (array_key_exists($setName, $blacklists)): ?>
+      <?php if ($showElementSetHeadings): ?>
+          <h2><?php echo html_escape(__($setName)); ?></h2>
+      <?php endif; ?>
+      <?php foreach ($setElements as $elementName => $elementInfo): ?>
+          <?php if ( !in_array($elementName, $blacklists[$setName], true ) ) { ?>
+              <div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
+                  <h3><?php echo html_escape(__($elementName)); ?></h3>
+                  <?php foreach ($elementInfo['texts'] as $text): ?>
+                      <div class="element-text"><?php echo $text; ?></div>
+                  <?php endforeach; ?>
+              </div><!-- end element -->
+          <?php } ?>
+      <?php endforeach; ?>
+    <?php endif; ?>
+  </div><!-- end element-set -->
 <?php endforeach;
